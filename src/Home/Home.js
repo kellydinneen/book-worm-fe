c231815fbf0bed21508c87de3c78db7af6fd2144
conconartist <conconart.info@gmail.com>
2021-04-07 18:54:44 -0600

    Update format

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -10,9 +10,14 @@ export const Home = () => {
     const [displayNewBookForm, setDisplayNewBookForm] = useState(false)
     return (
         <main>
-            <button onClick={()=>setDisplayNewBookForm(true)}>Add a Book</button>
+            <button 
+              onClick={()=>setDisplayNewBookForm(true)}
+            >Add a Book
+            </button>
             <button>See Finished Books</button>
-            <BookContainer data={data}/>
+            <BookContainer 
+              data={data}
+            />
             {displayNewBookForm && <NewBookForm setDisplay={setDisplayNewBookForm}/>}
         </main>
     )
