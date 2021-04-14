413b4e5e15337e97e5abb127674f3fb8e3fb4aaa
Thao Ma <MaThao@gmail.com>
2021-04-13 18:48:30 -0600

    Add button img and styled

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import apple from '../assets/apple.svg'
+import star from '../assets/star.svg'
+
 
 export const Header = () => {
 
   return (
-    <header>
-      
+    <header className='header-wrapper'>
+      <h1>BookWorm</h1>
+      <h2>Hi, Student</h2>
+      <img 
+        className='button-img'
+        src={star}
+      />
+      <img 
+        className='button-img'
+        src={apple}
+      />
     </header>
   )
 }
