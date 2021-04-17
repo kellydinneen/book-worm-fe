a3b3856b0d0951252fb083c06402de46715270a6
conconartist <conconart.info@gmail.com>
2021-04-17 16:20:23 -0600

    Render image to logout button

--- src/Logout/Logout.js
+++ src/Logout/Logout.js
@@ -1,22 +1,27 @@
 import React from 'react';
-import { GoogleLogout } from 'react-google-login';
+import { useGoogleLogout } from 'react-google-login';
+import apple from '../assets/apple.svg';
 
 const clientId = '426129823464-ckm4t40qqinikh5e96pvna36i4tujlo5.apps.googleusercontent.com';
 
 function Logout() {
-    const onSuccess = () => {
+    const onLogoutSuccess = (res) => {
         alert('Logout was successful!');
          
     };
-
+    const onFailure = () => {
+        console.log('Logout failed')
+    }
+    const { signOut } = useGoogleLogout({
+        clientId,
+        onLogoutSuccess,
+        onFailure,
+    });
     return (
-        <div>
-            <GoogleLogout
-              clientId={clientId}
-              buttonText='Logout'
-              onLogoutSuccess={onSuccess}
-            ></GoogleLogout>
-        </div>
+        <button onClick={signOut} className="logoutButton">
+            <img src={apple}></img>
+            <p>Logout</p>
+        </button>
     );
 }
 
