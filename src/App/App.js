f7de67af64e0e55e775cf70041e38980f9c40b31
conconartist <conconart.info@gmail.com>
2021-04-17 15:46:25 -0600

    Update App with Login and Logout components

--- src/App/App.js
+++ src/App/App.js
@@ -3,14 +3,26 @@ import { Home } from '../Home/Home';
 import { Header } from '../Header/Header';
 import BookDetails from '../BookDetails/BookDetails';
 import { Switch, Route } from 'react-router-dom';
+import Login from '../Login/Login';
+import Logout from '../Logout/Logout';
 
 const App = () => {
   return(
     <React.Fragment>
+
       <Header />
       <Switch>
         <Route
           exact path='/'
+          render={() => (
+            <div>
+            <Login />
+            <Logout />
+            </div>
+            )}
+        />
+        <Route
+          exact path='/home'
           render={() => (
             <Home />
             )}
