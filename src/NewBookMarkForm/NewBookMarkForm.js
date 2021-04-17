fcc37dc9922d535e1135ddb6ff649c10235d02ce
kellydinneen <kgdinneen@gmail.com>
2021-04-17 11:23:35 -0600

    Reset values in bookmark form

--- src/NewBookMarkForm/NewBookMarkForm.js
+++ src/NewBookMarkForm/NewBookMarkForm.js
@@ -2,15 +2,35 @@ import React, { useState } from 'react';
 import { useForm } from "react-hook-form";
 
 export const NewBookMarkForm = ({ book }) => {
-    const { register, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data);
+  const { register, reset, handleSubmit, formState: { errors } } = useForm();
 
-    return(
-      <form onSubmit={handleSubmit(onSubmit)}>
-         <input {...register("reading minutes", { required: true })} />
-         <input type="number" {...register("ending page", { required: true, min: 18, max: book.pages })} />
-         <input {...register("notes", { maxLength: 500 })} />
-         <input type="submit" />
-      </form>
-    )
+  const defaultValues = {
+    select: "",
+    input: ""
+  };
+
+  const onSubmit = data => {
+    console.log(data)
+    reset({ defaultValues })
+  };
+  console.log(errors);
+
+  return (
+   <form onSubmit={handleSubmit(onSubmit)}>
+     <input type="number" placeholder="What page did you finish on?" {...register("What page did you finish on?", {required: true})} />
+     <input type="number" placeholder="How many minutes did you read for?" {...register("How many minutes did you read for?", {required: true})} />
+     <input type="text" placeholder="Write down any thoughts or notes you have:" {...register} />
+     <select {...register}>
+        <option value="😮">😮</option>
+        <option value=" 🧐"> 🧐</option>
+        <option value=" 😆"> 😆</option>
+        <option value=" 😢"> 😢</option>
+        <option value=" 😡"> 😡</option>
+        <option value=" 😨"> 😨</option>
+        <option value=" 😊"> 😊</option>
+        <option value=" 🥰"> 🥰</option>
+      </select>
+     <input type="submit" />
+   </form>
+  );
 }
