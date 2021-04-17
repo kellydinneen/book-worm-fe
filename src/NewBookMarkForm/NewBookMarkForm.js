6e1d55fe11b809b4cb05dfa526132bdcb455ed9c
kellydinneen <kgdinneen@gmail.com>
2021-04-16 20:11:29 -0600

    Use react-hook-form for new bookmarks

--- src/NewBookMarkForm/NewBookMarkForm.js
+++ src/NewBookMarkForm/NewBookMarkForm.js
@@ -1,56 +1,16 @@
 import React, { useState } from 'react';
+import { useForm } from "react-hook-form";
 
-export const NewBookMarkForm = () => {
-    const [minutesRead, setMinutesRead] = useState(0);
-    const [endingPage, setEndingPage] = useState(0);
-    const [notes, setNotes] = useState('');
-    const [reactions, setReactions] = useState('');
+export const NewBookMarkForm = ({ book }) => {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => console.log(data);
 
     return(
-        <form>
-          <label>
-          How many minutes did you read?
-            <input
-              aria-label="reading minutes input"
-              className="reading-minutes-input"
-              placeholder="minutes spent reading"
-              value={minutesRead}
-              onChange={event => setMinutesRead(event.target.value)}>
-            </input>
-          </label>
-          <label>
-          What page did you end on?
-            <input
-              aria-label="ending page input"
-              className="ending-page-input"
-              placeholder="page number of the last page you read"
-              value={endingPage}
-              onChange={event => setEndingPage(event.target.value)}>
-            </input>
-          </label>
-          <label>
-          Any thoughts or notes?
-            <input
-              aria-label="notes input"
-              className="notes-input"
-              placeholder="What did you think of what you read?"
-              value={notes}
-              onChange={event => setNotes(event.target.value)}>
-            </input>
-          </label>
-          <label>
-          Reactions?
-            <input
-              aria-label="reactions-input"
-              className="reactions-input"
-              placeholder="choose an emoji"
-              value={reactions}
-              onChange={event => setReactions(event.target.value)}>
-            </input>
-          </label>
-          <button>
-              Submit Bookmark
-          </button>
-        </form>
+      <form onSubmit={handleSubmit(onSubmit)}>
+         <input {...register("reading minutes", { required: true })} />
+         <input type="number" {...register("ending page", { required: true, min: 18, max: book.pages })} />
+         <input {...register("notes", { maxLength: 500 })} />
+         <input type="submit" />
+      </form>
     )
 }
