ded738183f3648183543ab630cc93aa9974fce5c
kellydinneen <kgdinneen@gmail.com>
2021-04-17 11:05:35 -0600

    Add Book Details content

--- src/BookDetails/BookDetails.js
+++ src/BookDetails/BookDetails.js
@@ -1,27 +1,34 @@
 import React, { useState } from 'react';
 import { NewBookMarkForm } from '../NewBookMarkForm/NewBookMarkForm';
-import { useLocation, withRouter } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 
 const BookDetails = (props) => {
   const [displayNewBookMarkForm, setDisplayNewBookMarkForm] = useState(false);
   const book = props.location.state.book;
-  console.log(book);
 
     return(
         <section>
-          <button>Back to Current Books</button>
+          <Link to='/'>
+            <button>Back to Current Books</button>
+          </Link>
           <article>
+            <img className='book-details-image' src={book.attributes.image}/>
             <h2>{book.attributes.title}</h2>
             <h3>by {book.attributes.author}</h3>
             <p>{book.attributes.pages} pages long</p>
           </article>
           <article>
-            <div>PROGRESS BAR</div>
-            <button>Add A Bookmark</button>
+            <div>
+              <h2>Bookmarks</h2>
+              {displayNewBookMarkForm && <>
+                <NewBookMarkForm book={book}/>
+                <button onClick={() => setDisplayNewBookMarkForm(false)}>Cancel</button>
+              </>}
+              {!displayNewBookMarkForm && <button onClick={() => setDisplayNewBookMarkForm(true)}>Add A Bookmark</button>}
+            </div>
             <button>Finish Book</button>
             <button>Abandon Book</button>
           </article>
-          {displayNewBookMarkForm && <NewBookMarkForm />}
         </section>
     )
 }
