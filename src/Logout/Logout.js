c57d3509789a14e5bc2f36c58c992f23960e7574
conconartist <conconart.info@gmail.com>
2021-04-18 13:43:20 -0600

    Refactor Logout component

--- src/Logout/Logout.js
+++ src/Logout/Logout.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useGoogleLogout } from 'react-google-login';
+import { Redirect } from 'react-router-dom';
 import apple from '../assets/apple.svg';
+import Login from '../Login/Login';
+
+
 
 const clientId = '426129823464-ckm4t40qqinikh5e96pvna36i4tujlo5.apps.googleusercontent.com';
 
-function Logout() {
-    const onLogoutSuccess = (res) => {
-        alert('Logout was successful!');
-         
+function Logout({googleId}) {
+    console.log(googleId)
+    const [currentUser, setCurrentUser] = useState(googleId)
+    console.log("beforeLogout", currentUser)
+    const onLogoutSuccess = (res) => {     
+        setCurrentUser({});
+        console.log("logoutUser", currentUser)
+        console.log('Logout was successful!');
     };
     const onFailure = () => {
         console.log('Logout failed')
@@ -19,7 +27,8 @@ function Logout() {
     });
     return (
         <React.Fragment>
-            <img className="logOutButton" onClick={signOut} src={apple}></img>
+          <img className="logOutButton" onClick={signOut} src={apple}></img>
+          {currentUser === {} && <Redirect exact from="/home" to="/" />}
         </React.Fragment>
     );
 }
