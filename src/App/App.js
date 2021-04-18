e6e73985043c662c0bd5d95b25b8390e9fe1f21b
conconartist <conconart.info@gmail.com>
2021-04-17 19:47:25 -0600

    Access data from login to pass to Home component

--- src/App/App.js
+++ src/App/App.js
@@ -17,7 +17,6 @@ const App = () => {
           render={() => (
             <div>
             <Login />
-            {/* <Logout /> */}
             </div>
             )}
         />
--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useGoogleLogin } from 'react-google-login';
 import { refreshTokenSetup } from '../utils/refreshToken';
+import { Home } from '../Home/Home';
 import wormImg from '../assets/worm.png';
 
 const clientId = '426129823464-ckm4t40qqinikh5e96pvna36i4tujlo5.apps.googleusercontent.com';
 
 function Login() {
+    const [currentUser, setCurrentUser] = useState({})
     const onSuccess = (res) => {
         console.log('[Login Success] currentUser:', res.profileObj);
+        setCurrentUser(res.profileObj)
         refreshTokenSetup(res);
     };
     const onFailure = (res) => {
@@ -21,7 +24,8 @@ function Login() {
         accessType: 'offline',
     })
     return (
-        <main>
+        <React.Fragment>
+        {currentUser && <main>
           <div className="loginBackground">
             <div className="loginContainer">
               <img className="worm" src={wormImg}></img>
@@ -31,6 +35,11 @@ function Login() {
             </div>
           </div>
         </main>
+        }   
+       {/* <Home currentUser={currentUser}/> */}
+        </React.Fragment>
+        
+        
     )
 }
 
