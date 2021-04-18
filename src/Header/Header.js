7e5d89b06ba42daa25adc3333803303b45306363
conconartist <conconart.info@gmail.com>
2021-04-17 22:29:57 -0600

    Refactor Header component to center

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import appleImg from '../assets/apple.svg';
 import homeBtnImg from '../assets/home.svg';
 import wormImg from '../assets/worm.png'
 import Logout from '../Logout/Logout'
@@ -9,18 +8,21 @@ export const Header = () => {
   return (
     <header className='header-wrapper'>
       <div className='header-container'>
-          <h1 className="titleCloud">
-            BookWorm
-            <img className='worm-img' src={wormImg} alt='worm'/>
-          </h1>
-      </div>
-      <h2 className='greeting titleCloud'>Hi, Student</h2>
       <img 
           alt='home button'
           className='button-img'
           src={homeBtnImg}
         />
+        <div className='titleContainer'>
+          <h1>
+            BookWorm
+          </h1>
+          <img className='worm-img' src={wormImg} alt='worm'/>
+        </div>
       <Logout />
+      </div>
+      <h2 className='greeting'>Hi, Student</h2>
+      
     </header>
   )
 }
