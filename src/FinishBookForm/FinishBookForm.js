13987c895a9566316c8c99a45c2ae272895caf89
Thao Ma <MaThao@gmail.com>
2021-04-18 16:04:53 -0600

    Add StarRating Component

--- src/FinishBookForm/FinishBookForm.js
+++ src/FinishBookForm/FinishBookForm.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import { markBookFinished } from '../apiCalls.js';
+import { StarRating } from '../StarRating/StarRating';
 
 export const FinishBookForm = ({ book }) => {
-    const { register, handleSubmit, formState: { errors } } = useForm();
+    const { register, handleSubmit } = useForm();
 
     const submitBookReview = async (data) => {
       const bookReview = {
@@ -15,27 +16,32 @@ export const FinishBookForm = ({ book }) => {
         review_comment: data.review
       }
       const result = await markBookFinished(bookReview);
-      console.log(result);
       return result;
     }
 
     const onSubmit = data => submitBookReview(data);
-    console.log(errors);
 
     return (
       <form className='finish-form' onSubmit={handleSubmit(onSubmit)}>
-        <label className='finish-form-label'>How did you like it on a scale from 1 to 5?</label>
-        <select {...register("rating", { required: true })}>
+        <label>How did you like it on a scale from 1 to 5?</label>
+        <StarRating totalStars={5}/>
+        {/* <select {...register("rating", { required: true })}>
           <option value="1">⭐️</option>
           <option value="2"> ⭐️⭐️</option>
           <option value="3"> ⭐️⭐️⭐️</option>
           <option value="4"> ⭐️⭐️⭐️⭐️</option>
           <option value="5"> ⭐️⭐️⭐️⭐️⭐️</option>
-        </select>
-        <label className='finish-form-label'>Tell your friends a little bit about it:</label>
-        <input type="text" placeholder="review" {...register("review", {required: true, maxLength: 500})} />
-
-        <input type="submit" />
+        </select> */}
+        <label>Tell your friends a little bit about it:</label>
+        <input 
+          type="text" 
+          placeholder="review" 
+          {...register("review", {required: true, maxLength: 500})} 
+        />
+        <button 
+          className='finished-book-submit-button'
+          type="submit">Submit
+        </button>
       </form>
     );
 }
