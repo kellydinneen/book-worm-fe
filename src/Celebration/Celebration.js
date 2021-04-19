d4fd9b9c131267e3c88927fbe2a7f9458a7236eb
conconartist <conconart.info@gmail.com>
2021-04-19 08:37:51 -0600

    Update animation

--- src/Celebration/Celebration.js
+++ src/Celebration/Celebration.js
@@ -12,17 +12,17 @@ import { gsap, CSSPlugin } from 'gsap';
 gsap.registerPlugin(CSSPlugin);
 
 const FinishedBook = ({currentUser}) => {
-    CSSPlugin.defaultTransformPerspective = 400;
+    CSSPlugin.defaultTransformPerspective = 300;
     gsap.to(".dancingWorm", {x: -150, duration:5, rotation:360})
     gsap.to(".dancingWorm", {x: 150, duration:5, scale: 1.2, ease: "bounce"})
     gsap.to(".dancingBook", {x: 150, duration:5, rotation:360})
     gsap.to(".dancingBook", {x: -150, duration:5, scale: 1.2, ease: "bounce"})
     gsap.to(".flower", {x: -40, y: 400, duration: 5, rotation:260, ease: "bounce"})
     gsap.to(".sushi", {x: -90, y: 465, duration: 6, rotation:360, ease: "bounce"})
-    gsap.to(".apple", {y: 440, duration: 3, rotation:60, ease: "bounce"})
+    gsap.to(".apple", {y: 440, duration: 8, rotation:60, ease: "bounce"})
     gsap.to(".donut", {x: 80, y: 460, duration: 4, rotation:360, ease: "bounce"})
     gsap.to(".redBalloon", {y: -360, duration: 6, rotation: 40, ease: "bounce"})
-    gsap.to(".yellowBalloon", {x: 100, y: -340, duration: 5, rotation: 360, ease: "bounce"})
+    gsap.to(".yellowBalloon", {x: 100, y: -340, duration: 9, rotation: 340, ease: "bounce"})
   
     return(
       <React.Fragment>
