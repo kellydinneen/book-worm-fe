6b4e22a791a629ec13a9d9b76c86a7f4241567be
kellydinneen <kgdinneen@gmail.com>
2021-04-18 20:31:08 -0600

    Create App test

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,8 +1,14 @@
+import React from "react";
 import { render, screen } from '@testing-library/react';
+import { shallow, mount } from "enzyme";
+import toJson from "enzyme-to-json";
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it("renders correctly", () => {
+  const wrapper = mount(<App />);
+  expect(wrapper.state("error")).toEqual(null);
+});
+
+it("renders without crashing", () => {
+  shallow(<App />);
 });
