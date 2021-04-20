94914b592f1015e5305750594d966a2f4c367ab8
conconartist <conconart.info@gmail.com>
2021-04-20 14:37:28 -0600

    Update finished book page

--- src/FinishedBookCard/FinishedBookCard.js
+++ src/FinishedBookCard/FinishedBookCard.js
@@ -6,11 +6,11 @@ export const FinishedBookCard = ({ book, bookMarks }) => {
   console.log(book);
 
   const bookMarkList = () => bookMarks.map(mark =>
-    <Collapsible trigger={mark.attributes.reactions ?
+    <Collapsible className='finishedBookmarks' trigger={mark.attributes.reactions ?
       moment(mark.attributes.date).format('LL') + '......' + mark.attributes.reactions :
       moment(mark.attributes.date).format('LL') + '......' + '🍎'
     }>
-      <section key={mark.id}>
+      <section className='finishedBookmarks' key={mark.id}>
         <p>I read for {mark.attributes.minutes} mins!</p>
         <p> On page: {mark.attributes.page_number}</p>
         <p>Notes: {mark.attributes.notes}</p>
@@ -21,7 +21,7 @@ export const FinishedBookCard = ({ book, bookMarks }) => {
     <section className='finished-book-card'>
       <h2 className='finished-book-title'>{book.attributes.title}</h2>
       <img className='finished-book-img' src={book.attributes.image} alt='book cover'/>
-      <h3>Bookmarks</h3>
+      <h3 className='bookmarkHeading'>Bookmarks</h3>
       <div className='finished-bookmarks'>
         {!bookMarks.length ? <p>You didn't leave any bookmarks in this one</p> : bookMarkList()}
       </div>
