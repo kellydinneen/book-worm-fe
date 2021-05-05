62cd26f48c8fa301085d9692106a72261ef4edb2
Thao Ma <MaThao@gmail.com>
2021-05-05 16:50:49 -0600

    Finish friends

--- src/FriendList/FriendList.js
+++ src/FriendList/FriendList.js
@@ -16,7 +16,7 @@ export const FriendList = () => {
         />
         <p><b>Catherine Dean</b></p>
         <p className='booklist'><b>Currently Reading:</b>
-          <span className='current-books-list'>Harry Potter and the Sorcerer's Stone, The Giver</span>
+          <span className='current-books-list'>Harry Potter and the Sorcerer's Stone, The Giver, The Catcher in the Rye </span>
         </p>
       </div>
       <div className='friend-card'>
@@ -26,7 +26,21 @@ export const FriendList = () => {
         />
         <p><b>Joe Jiang</b></p>
         <p className='booklist'><b>Currently Reading:</b>
-          <span className='current-books-list'>Harry Potter and the Chamber of Secrets</span>
+          <span className='current-books-list'>Harry Potter and the Chamber of Secrets, Fahrenheit 451</span>
+        </p>
+      </div>
+      <div className='friend-card'>
+        <img className='bookworm-img' src={BookWormBeanie} alt='avatar'/>
+        <p><b>Kelly Dinneen</b></p>
+        <p className='booklist'><b>Currently Reading:</b>
+          <span className='current-books-list'>Harry Potter and the Prisoner of Azkaban, Anne of Green Gables, Little Women, Jane Erye </span>
+        </p>
+      </div>
+      <div className='friend-card'>
+        <img className='bookworm-img' src={BookWormShoes} alt='avatar'/>
+        <p><b>Thao Ma</b></p>
+        <p className='booklist'><b>Currently Reading:</b>
+          <span className='current-books-list'>Harry Potter and the Goblet of Fire, The Great Gatsby, The Fault in Our Stars, The Hate U Give, The Hunger Games</span>
         </p>
       </div>
     </section>
